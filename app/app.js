bbf64be574391027bfec5e217b4da966003666a8
Niraj Gupta <nkg448@gmail.com>
2017-02-06 14:22:34 -0600

    Modifying MERN skeleton files

--- app/app.js
+++ app/app.js
@@ -1,12 +1,9 @@
-import React from 'react';
-import {render} from 'react-dom';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-import Main from './Main'; // Our custom react component
+// Include the Main React Dependencies
+var React = require("react");
+var ReactDOM = require("react-dom");
 
-// Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
-injectTapEventPlugin();
+// Include the main Main Component
+var Main = require("./components/Main");
 
-// Render the main app react component into the app div.
-// For more details see: https://facebook.github.io/react/docs/top-level-api.html#react.render
-render(<Main />, document.getElementById('app'));
+// This code here allows us to render our main component (in this case Main)
+ReactDOM.render(<Main />, document.getElementById("app"));
