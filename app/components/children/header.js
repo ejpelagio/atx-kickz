e3d7bda336bfcb789695068dd8e126406014d456
ejpelagio <ejpelagio@yahoo.com>
2017-02-07 11:53:47 -0600

    initial header JS

---
+++ app/components/children/header.js
@@ -0,0 +1,25 @@
+// Include React
+var React = require("react");
+
+// This is the History component. It will be used to show a log of  recent searches.
+var Navbar = React.createClass({
+  // Here we describe this component's render method
+  render: function() {
+    return (
+    	<div class="page-header">
+      <ul class="nav nav-tabs">
+  	<li role="presentation" class="active"><a href="#">My Kicks</a></li>
+  	<li role="presentation"><a href="#">Browse</a></li>
+  	<li role="presentation"><a href="#">Marketplace</a></li>
+  	<li role="presentation"><a href="#">New Releases</a></li>
+	</ul>
+
+          </div>
+            
+    );
+  }
+});
+
+
+// Export the component back for use in other files
+module.exports = Navbar;
