fa71602751a439eb06c0507a02e95a4da38d7a90
Evan Anderson <epanderson92@yahoo.com>
2017-02-13 14:00:02 -0600

    marketplace modification

---
+++ app/components/views/marketplace.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+export default class Marketplace extends React.Component {
+    render(){
+        return (
+
+
+        	<h1>Helloo from the Marketplace page!</h1>
+
+
+
+
+
+
+        	);
+    }
+}
