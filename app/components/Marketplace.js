9c0ae8fdfc8271650846bf38531ccae0c985cf5f
Evan Anderson <epanderson92@yahoo.com>
2017-02-14 13:20:25 -0600

    market place card in progress

--- app/components/Marketplace.js
+++ app/components/Marketplace.js
@@ -4,12 +4,19 @@ import React from 'react';
 export default class Marketplace extends React.Component {
     render(){
         return (
+        <div>
+          <h1><b>Market Place</b></h1>
 
 
-        	<h1>Helloo from the Marketplace page!</h1>
-
-
-
+<div class="card" style="width: 20rem;">
+  <img class="card-img-top" src="..." alt="Card image cap"/>
+  <div class="card-block">
+    <h4 class="card-title">Card title</h4>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    {/*<link href="#" class="btn btn-primary">Go somewhere</link>*/}
+  </div>
+</div>
+</div>
 
 
 
