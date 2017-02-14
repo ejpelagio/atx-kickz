b3bbecb8dffea41d248718b9f395d2c310ac59f4
Niraj Gupta <nkg448@gmail.com>
2017-02-14 16:26:33 -0600

    image uploads are working

--- app/components/children/FormsyTestv1.js
+++ app/components/children/FormsyTestv1.js
@@ -5,15 +5,42 @@ const { Form, Input, File, RadioGroup, Checkbox, CheckboxGroup, Select } = FRC;
 
 var FormsyTest = React.createClass({
   // Here we describe this component's render method
+  getInitialState: function() {
+    return {
+      imageURL: ""
+    };
+  },
+
+  componentWillMount: function() {
+        const script = document.createElement("script");
+        script.src = "//widget.cloudinary.com/global/all.js";
+        script.type = 'text/javascript';
+        script.async = true;
+
+        document.body.appendChild(script);
+    },
+
+  handleUpload: function() {
+    cloudinary.openUploadWidget({ cloud_name: 'dawscxdsi', upload_preset: 'pjmooj37'}, 
+      function(error, result) { 
+        console.log(result[0].secure_url);
+        this.setState({imageURL: result[0].secure_url});
+       }.bind(this));
+  }, 
+
   render: function() {
         
         const multiOptions = [
+            {label: '8', value: '8'},
+            {label: '9', value: '9'},
             {label: '10', value: '10'},
             {label: '11', value: '11'},
             {label: '12', value: '12'}
         ];
 
         const submitForm = (data) => {
+        console.log(this.state.imageURL);
+        data.imageURL=this.state.imageURL;
         this.props.setShoeData(data);
         console.log(data); 
         }
@@ -25,52 +52,38 @@ var FormsyTest = React.createClass({
                     <Input
                         label="Brand"
                         name="brand"
-                        componentRef={(component) => { this.inputRef = component; } }
+                        
                     />
 
                     <Input
                         label="Year"
                         name="year"
-                        componentRef={(component) => { this.inputRef = component; } }
+                        
                     />
                     <Input
                         label="Model"
                         name="model"
-                        componentRef={(component) => { this.inputRef = component; } }
+                        
                     />
                     <Input
                         label="Color"
                         name="color"
-                        componentRef={(component) => { this.inputRef = component; } }
+                        
                     />
 
                     <Select
                         label="Shoe Size"
                         name="size"
                         options={multiOptions}
-                        componentRef={(component) => { this.selectRef = component; } }
+                        
                     />
                     
-                    {/*
-                    <File
-                        label="Image URL"
-                        name="file"
-                        componentRef={(component) => { this.fileRef = component; } }
-                    />*/}
-                    
-                    <Checkbox
-                        label="Single checkbox"
-                        valueLabel="One option"
-                        name="checkbox"
-                        componentRef={(component) => { this.checkboxRef = component; } }
-                    />
-                    <Select
-                        label="Select"
-                        name="select"
-                        options={multiOptions}
-                        componentRef={(component) => { this.selectRef = component; } }
-                    />
+                    <div className="container">
+                    <button type="button" className="btn btn-default" onClick={this.handleUpload}>Upload</button> 
+                    <span>  </span>
+                                        
                     <input className="btn btn-primary" formNoValidate={true} type="submit" defaultValue="Submit" />
+                    </div>
                 </Form>
 
             </div>
