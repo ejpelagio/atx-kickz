1d9cfc535e236b3248bf672efa14f63b494dca35
Evan Anderson <epanderson92@yahoo.com>
2017-02-14 17:42:27 -0600

    first card in the Marketplace.js.  MarketplaceCard.js added

--- app/components/Marketplace.js
+++ app/components/Marketplace.js
@@ -1,25 +1,22 @@
-import React from 'react';
-
+import React from "react";
+var MarketplaceCard = require("./children/MarketplaceCard");
 
 export default class Marketplace extends React.Component {
     render(){
         return (
         <div>
           <h1><b>Market Place</b></h1>
-
-
-<div class="card" style="width: 20rem;">
-  <img class="card-img-top" src="..." alt="Card image cap"/>
-  <div class="card-block">
-    <h4 class="card-title">Card title</h4>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    {/*<link href="#" class="btn btn-primary">Go somewhere</link>*/}
-  </div>
-</div>
-</div>
+          <MarketplaceCard />;
+        </div>
 
 
 
-        	);
+        );
     }
+
+
+
+
+
+
 }
