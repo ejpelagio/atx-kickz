4e6f2bdae8dfbaeb3b3aed03f40b5633c71cc1cc
Evan Anderson <epanderson92@yahoo.com>
2017-02-15 18:58:50 -0600

    cards are dynamically created with images

--- app/components/Marketplace.js
+++ app/components/Marketplace.js
@@ -1,15 +1,69 @@
-import React from "react";
+var React = require("react");
+var helpers = require("../utils/helpers");
 var MarketplaceCard = require("./children/MarketplaceCard");
+var AllShoes = require("./children/AllShoes");
 
-export default class Marketplace extends React.Component {
-    render(){
+
+var Marketplace = React.createClass ({
+  getInitialState: function() {
+    return { allShoes: [], shoeData: "" };
+  },
+
+  // The moment the page renders get the History
+  componentDidMount: function() {
+    // Get the latest history.
+
+    helpers.getShoes().then(function(response) {
+      console.log(response);
+      if (response !== this.state.allShoes) {
+        this.setState({ allShoes: response.data });
+      }
+      console.log("Check state", this.state.allShoes);
+    }.bind(this));
+    console.log("After mount: ",this.state);
+  },
+
+  // If the component changes (i.e. if a search is entered)...
+  componentDidUpdate: function(prevProps, prevState) {
+    if(this.state.shoeData!=prevState.shoeData){
+      helpers.postShoes(this.state.shoeData).then(function(){
+        console.log("posted!");
+
+        helpers.getShoes().then(function(response) {
+          console.log("shoelist: ", response.data);
+          this.setState({ allShoes: response.data});
+        }.bind(this));
+
+      }.bind(this));
+    }
+
+  },
+
+
+    render: function(){
         return (
         <div>
           <h1><b>Market Place</b></h1>
           <MarketplaceCard />;
+          <div className="row">
+            {this.state.allShoes.map(function(search, i) {
+              return (
+                <div className="col-md-3">
+                  <MarketplaceCard title={search.brand} image={search.imageURL} />
+                </div>
+              );
+            })}
+          </div>
+
+
+          <AllShoes allShoes={this.state.allShoes} />
+
         </div>
-)
+
+      )
 
 
 }
-};
+});
+
+module.exports=Marketplace;
--- app/components/children/MarketPlaceCard.js
+++ app/components/children/MarketPlaceCard.js
@@ -6,9 +6,9 @@ var MarketplaceCard = React.createClass({
 render: function(){
   return(
 <div className="card" style={{width: "20rem"}}>
-  <img className="card-img-top" src="..." alt="Card image cap"/>
+  <img className="card-img-top" src={this.props.image} alt="Card image cap"/>
   <div className="card-block">
-    <h4 className="card-title">Card title</h4>
+    <h4 className="card-title">{this.props.title}</h4>
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
