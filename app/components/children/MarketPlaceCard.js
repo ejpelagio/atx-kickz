057dadeac216c31f1314100fb408e2d972957564
Will Palafox <wpalafox@gmail.com>
2017-02-17 13:29:13 -0600

    more merge fixes pt2

---
+++ app/components/children/MarketPlaceCard.js
@@ -0,0 +1,30 @@
+// include React
+var React = require("react");
+
+// Market place createClass
+var MarketplaceCard = React.createClass({
+render: function(){
+  return(
+
+
+		<div className="card" style={{width: "20rem"}}>
+		  <img className="card-img-top" src={this.props.image} alt="Card image cap"/>
+		  <div className="card-block">
+		    <h4 className="card-title">{this.props.title}</h4>
+		    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+		    <a href="#" class="btn btn-primary">Go somewhere</a>
+		  </div>
+		</div>
+	
+
+
+	);
+   }
+
+
+
+
+});
+
+// exports market place card
+module.exports = MarketplaceCard;
